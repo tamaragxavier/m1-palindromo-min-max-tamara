f265d4bfa302af94b007438b750ac1c4ebbe5bc9
Tamara Xavier <tamaragiliski96@gmail.com>
2024-09-23 12:08:44 -0300

    tarefa 1

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    str = str.toLowerCase().replaceAll(" ", "");
+    let strInvertida = "";
+  
+    for (let i = str.length - 1; i >= 0; i--) {
+      strInvertida += str[i];
+    }
+    if (strInvertida == str) {
+      return true;
+    }
+    return false;
 }
+
+console.log(isPalindrome("A sacada da casa"));
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
