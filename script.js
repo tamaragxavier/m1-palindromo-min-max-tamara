6f43bb645cb4df6a8621d4a264438f57c2291fac
Tamara Xavier <tamaragiliski96@gmail.com>
2024-09-23 12:10:54 -0300

    tarefa 2

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-function isPalindrome(str){
-    str = str.toLowerCase().replaceAll(" ", "");
-    let strInvertida = "";
-  
-    for (let i = str.length - 1; i >= 0; i--) {
-      strInvertida += str[i];
-    }
-    if (strInvertida == str) {
-      return true;
-    }
-    return false;
+function isPalindrome(str) {
+  str = str.toLowerCase().replaceAll(" ", "");
+  let strInvertida = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    strInvertida += str[i];
+  }
+  if (strInvertida == str) {
+    return true;
+  }
+  return false;
 }
 
 console.log(isPalindrome("A sacada da casa"));
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return [min, max];
 }
